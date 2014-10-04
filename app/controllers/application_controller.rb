0494ed66d02c18987fdadce6e76c90c6d346ccb2
Steve Marshall <sjmarshalluk@steves-air.home>
2014-10-04 15:20:07 +0100

    Show nav based on logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-  	User.find(session[:user_id])
+  	if session[:user_id].present?
+  		User.find(session[:user_id])
+  	end
   end
 
   def logged_in?
